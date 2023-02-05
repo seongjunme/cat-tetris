343464ce2f91d2bc93193ee33db49260fb180d30
dan.kk <dan.kk>
2023-02-05 17:47:34 +0900

    webpack & babel 세팅

---
+++ webpack.dev.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.jsx',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.([j|t]sx?$)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+};
